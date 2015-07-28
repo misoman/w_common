bb18f3eb51ca8a89f7056f964dab7ad16e118dec
Joel Handwell <joelhandwell@gmail.com>
2015-07-28 15:53:43 -0400

    Configure hostname

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
+default['set_fqdn'] = '*.examplewebsite.com'
 default['tz'] = 'America/New_York'
 default['ntp']['servers'] = ['0.us.pool.ntp.org', 'server 1.us.pool.ntp.org', 'server 2.us.pool.ntp.org', 'server 3.us.pool.ntp.org']
 default['apt']['compile_time_update'] = true
--- test/integration/test_w_common/serverspec/localhost/default_spec.rb
+++ test/integration/test_w_common/serverspec/localhost/default_spec.rb
@@ -8,7 +8,31 @@ describe 'w_comman::default' do
 	describe package('curl') do
 	  it { should be_installed }
 	end
-  
+
+  if os[:family] == 'ubuntu' && os[:release] == '12.04'
+    describe file('/etc/hostname') do
+      it { should exist }
+      its(:content) { should match /^test-w-common-ubuntu-1204$/ }
+    end
+
+    describe file('/etc/hosts') do
+      it { should exist }
+      it { should contain 'test-w-common-ubuntu-1204.examplewebsite.com' }
+    end
+  end
+
+  if os[:family] == 'ubuntu' && os[:release] == '14.04'
+    describe file('/etc/hostname') do
+      it { should exist }
+      its(:content) { should match /^test-w-common-ubuntu-1404$/ }
+    end
+
+    describe file('/etc/hosts') do
+      it { should exist }
+      it { should contain 'test-w-common-ubuntu-1404.examplewebsite.com' }
+    end
+  end
+
   # timezone should be est or edt
   describe command('date +%Z') do
     its(:stdout) { should match(/(EST|EDT)/) }
@@ -17,5 +41,5 @@ describe 'w_comman::default' do
   describe port(22) do
   	it { should be_listening.with('tcp') }
 	end
-	
+
 end
