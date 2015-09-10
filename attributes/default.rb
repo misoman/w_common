0fde20c73e97d2ded5feebba21cdeeacd1c2b877
Joel Handwell <joelhandwell@gmail.com>
2015-09-10 16:35:47 -0400

    Add default chefserver hostfile entry

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,5 @@
+default['chefserver']['ip'] = '127.0.0.1'
+default['chefserver']['hostname'] = 'chefserver.examplewebsite.com'
 default['set_fqdn'] = '*.examplewebsite.com'
 default['tz'] = 'America/New_York'
 default['ntp']['servers'] = ['0.us.pool.ntp.org', 'server 1.us.pool.ntp.org', 'server 2.us.pool.ntp.org', 'server 3.us.pool.ntp.org']
