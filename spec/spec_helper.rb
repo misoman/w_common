e7980f7a63900981c65bedc02dcf094dad714329
Joel Handwell <joelhandwell@gmail.com>
2015-12-30 23:52:19 +0900

    Proper chefspec test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,19 +3,20 @@ require 'chefspec/berkshelf'
 require 'mymatchers'
 
 ChefSpec::Coverage.start! do
-  add_filter(%r{/ntp/})
-  add_filter(%r{/sudo/})
-  add_filter(%r{/timezone-ii/})
-  add_filter(%r{/apt/})
-  add_filter(%r{/monit/})
-  add_filter(%r{/build-essential/})
-  add_filter(%r{/git/})
-  add_filter(%r{/apt-repo/})
-  add_filter(%r{/hostname/})
+  add_filter(%r{ntp})
+  add_filter(%r{sudo})
+  add_filter(%r{timezone-ii})
+  add_filter(%r{apt})
+  add_filter(%r{monit})
+  add_filter(%r{build-essential})
+  add_filter(%r{git})
+  add_filter(%r{apt-repo})
+  add_filter(%r{hostname})
+  add_filter(%r{firewall})
+  add_filter(%r{chef-sugar})
  end
 
 RSpec.configure do |config|
   config.platform = 'ubuntu'
   config.version = '14.04'
-  config.filter_run_excluding skip: true
 end
